808ea5687f2bb7415326af9992a3327c30f40224
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-03-08 11:54:04 +0200

    Initial Commit

---
+++ PagingScrollViewInSwiftUI/PagingScrollViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagingScrollViewInSwiftUIApp.swift
+//  PagingScrollViewInSwiftUI
+//
+//  Created by test on 08.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PagingScrollViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
